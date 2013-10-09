73c336af6a66199126197dc9498901689448351d
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:11:35 -0500

    Workday 6

--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,18 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim pop As Double
+        Dim Year As Integer
+        Year = 2012
+        pop = 7000000000
+        Do While pop > 6000000
+            'While the population is over 6M keep going if it is less stop
+            Year = Year - 50
+            pop = pop / 2
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Loop
+
+        MessageBox.Show("In " & Year & " the population was 6 million")
 
     End Sub
 End Class
